8bdf0543325152230be06ebf900b97548b41ffb7
Deivison Santos <138685992+DeivisonCs@users.noreply.github.com>
2024-11-03 21:58:57 -0300

    Update view.sql

--- TransportePublico/view.sql
+++ TransportePublico/view.sql
@@ -1,4 +1,6 @@
-CREATE MATERIALIZED VIEW vw_passagens_resumo AS
+DROP MATERIALIZED VIEW IF EXISTS view_passagens_resumo;
+
+CREATE MATERIALIZED VIEW view_passagens_resumo AS
     SELECT
         p.linha_id,
         COUNT(p.passageiro_id) AS total_passageiros,
@@ -10,3 +12,24 @@ CREATE MATERIALIZED VIEW vw_passagens_resumo AS
         pagamento pg ON p.pagamento_id = pg.pagamento_id
     GROUP BY
         p.linha_id, DATE(p.data_hora);
+
+REFRESH MATERIALIZED VIEW view_passagens_resumo;
+
+
+// ----------------- Exemplos de consultas ----------------- 
+
+SELECT * FROM view_passagens_resumo
+	WHERE data = '2024-11-01';
+ 	
+SELECT 
+    linha_id,
+    total_passageiros,
+    total_arrecadado
+FROM view_passagens_resumo
+WHERE data = '2024-11-01';
+
+SELECT 
+    total_passageiros,
+    total_arrecadado
+FROM view_passagens_resumo
+WHERE linha_id = 1;
