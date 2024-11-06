08abd3f7a3fe0867e1252224b27a0414e38f87e4
Deivison Santos <138685992+DeivisonCs@users.noreply.github.com>
2024-11-06 19:55:58 -0300

    Atvd Triggers

---
+++ Triggers/Trigger.sql
@@ -0,0 +1,27 @@
+SELECT * FROM indice;
+SELECT * FROM indice WHERE codmunicipio = 354880;
+	
+UPDATE indice SET idh_geral = 0.880 WHERE codmunicipio = 354880;
+
+---------------------- TRIGGERS ------------------------------
+
+DROP FUNCTION IF EXISTS RecalculateIDX CASCADE;
+DROP TRIGGER IF EXISTS updateIDX ON CASCADE;
+
+CREATE OR REPLACE FUNCTION RecalculateIDX()
+RETURNS TRIGGER AS $RecalculateIDX$
+BEGIN
+	UPDATE indice
+	SET idx = (NEW.idh_educacao^2 * NEW.idh_longevidade) / NEW.idh_geral
+	WHERE codmunicipio = NEW.codmunicipio;
+
+    RETURN NEW;
+END;
+$RecalculateIDX$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updateIDX
+AFTER UPDATE
+ON indice
+FOR EACH ROW
+WHEN (OLD.idx = NEW.idx)
+EXECUTE PROCEDURE RecalculateIDX();
