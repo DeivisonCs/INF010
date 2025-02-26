7fc199a9a7dcaf365e358edcdbfea7e79b5a444f
Deivison Santos <deivisonddj3@gmail.com>
2025-02-26 02:24:41 -0300

    turning readable

--- MiningAlgorithm/testes.py
+++ MiningAlgorithm/testes.py
@@ -20,11 +20,28 @@ def get_index_by_product(items, product_name):
 def encode_transactions(transactions):
     all_items = sorted(set(item for transaction in transactions for item in transaction))
     
-    encoded_data = []
-    for transaction in transactions:
-        encoded_data.append({item: (item in transaction) for item in all_items})
+    return all_items
+
+def iterate_items_in_transaction(transaction, items_dict, item):
+    for product in transaction:
+        if product != item:
+            product_index = get_index_by_product(items_dict[item]['items'], product)
+            
+            if product_index != -1:
+                items_dict[item]['items'][get_index_by_product(items_dict[item]['items'], product)]['qtd'] += 1
+            else:
+                items_dict[item]['items'].append({'product': product, 'qtd': 1})
     
-    get_patterns(all_items, transactions)
+    
+def search_in_transactions(transactions, items_dict, item):
+    for transaction in transactions:
+        # Se tiver o item na transação
+        if item in transaction:
+            items_dict[item]['qtd'] += 1
+            
+            # Percorre por cada produto da transação e guarda o nome e incrementa a quantidade de ocorrências desse produto
+            iterate_items_in_transaction(transaction, items_dict, item)
+            
     
 def get_patterns(all_items, transactions):
     items_dict = {}
@@ -34,24 +51,12 @@ def get_patterns(all_items, transactions):
         items_dict[item] = {'qtd':0, 'items': []}
         
         # Percorre as transações buscando se contem o item
-        for transaction in transactions:
-            
-            # Se tiver o item na transação
-            if item in transaction:
-                items_dict[item]['qtd'] += 1
-                
-                # Percorre por cada item da transação
-                for product in transaction:
-                    if product != item:
-                        product_index = get_index_by_product(items_dict[item]['items'], product)
-                        
-                        if product_index != -1:
-                            items_dict[item]['items'][get_index_by_product(items_dict[item]['items'], product)]['qtd'] += 1
-                        else:
-                            items_dict[item]['items'].append({'product': product, 'qtd': 1})
+        search_in_transactions(transactions, items_dict, item)
                             
     # print_dict(items_dict)
-    print_result(items_dict)
+    format_data(items_dict)
+    
+    return items_dict
     
 
 def get_support(x, y):
@@ -72,7 +77,7 @@ def get_confidence(data, x, y):
 
     return both_occurrence / x_occurrence
     
-def print_result(data):
+def format_data(data):
     print('antecedent\tconsequent\tsupport\tconfidence')
     output = []
     
@@ -81,5 +86,15 @@ def print_result(data):
             print(f'{item}\t{product['product']}\t{get_support(item, product['product'])}\t{round(get_confidence(data, item, product['product']), 2)}')
             output.append({'antecedent':item, 'consequent':product['product'], 'support':get_support(item, product['product']), 'confidence':round(get_confidence(data, item, product['product']), 2)})
 
+    return output
 
-encode_transactions(transactions)
+
+
+
+# ------------------------------------------------------------
+
+all_items = encode_transactions(transactions)
+data = get_patterns(all_items, transactions)
+formated_data = format_data(data)
+
+print(formated_data)
